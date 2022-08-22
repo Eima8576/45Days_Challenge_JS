388cda2ad341e0e31e5a462f4148945a192d7b4a
Eima8576 <90395382+Eima8576@users.noreply.github.com>
2022-08-22 22:29:30 +0545

    Add files via upload

---
+++ day3/operators.js
@@ -0,0 +1,50 @@
+//day 3 operators in JavaScript
+console.log("Operators in JS");
+let a = 30;
+let b = 3;
+// console.log("Arithmetic Operators");
+// console.log("a + b = ", a+b);
+// console.log("a - b = ", a-b);
+// console.log("a * b = ", a*b);
+// console.log("a / b = ", a/b);
+// console.log("a ** b = ", a**b);
+// console.log("a % b = ", a%b);
+// console.log("a++ = ", a++);
+// console.log("a-- = ", a--);
+// console.log("++a = ", ++a);
+// console.log("--a = ", --a);
+
+// console.log("Assignment Operatos");
+// a +=8
+// console.log(a);
+// a *=4
+// console.log(a);
+// a -=2
+// console.log(a);
+// a /=8
+// console.log(a);
+
+console.log("Comparison Operators")
+ 
+let comp1 = 9;
+let comp2 = "9";
+console.log("comp1 == comp2", comp1 == comp2);
+console.log("comp1 != comp2", comp1 != comp2);
+console.log("comp1 === comp2", comp1 === comp2);
+console.log("comp1 !== comp2", comp1 !== comp2);
+console.log("comp1 !== comp2", comp1 !== comp2);
+
+let try1 = 9;
+let try2 = 3;
+console.log("try1 >= try2", try1 >= try2);
+
+console.log("Logical Operators")
+
+let x = 3;
+let y = 8;
+console.log(x<y && y==8);
+console.log(x>y || y==8);
+console.log(!false);
+
+// comments in JS 
+
