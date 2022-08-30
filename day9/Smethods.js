1169d8090d0266c2b33c9523bf89f042a2864452
Eima8576 <90395382+Eima8576@users.noreply.github.com>
2022-08-30 18:54:20 +0545

    Add files via upload

---
+++ day9/Smethods.js
@@ -0,0 +1,23 @@
+//string methods
+// let me = "eima"
+// console.log(me.toUpperCase())
+// let name = "YIbO"
+// console.log(name.toLowerCase())
+// console.log(name.slice(1,3))
+// console.log(name.slice(3))
+// console.log(me.replace("ei", "Yi"))
+
+let name = "Yibo"
+let friend = "Zhan"
+// console.log(name.concat("is a friend of ", friend, " Ok "))
+
+// let friend2 = "       Xiao     "
+// console.log(friend2.trim())
+// console.log(friend2)
+
+// console.log(friend[3]);
+
+// use a for loop to print the string
+for(let i = 0; i<= name.length;i++){
+    console.log(name[i])
+}
---
+++ day9/strings.js
@@ -0,0 +1,24 @@
+// //collection of characters is string
+// let name = "Eima"
+// console.log(name)
+// console.log(name.length)
+// console.log(name[0])
+// console.log(name[1])
+// console.log(name[2])
+
+//Template literals
+//we can directly insert value to the variables in template literal.
+//this is called string interpolation
+let boy1 = "Dipen"
+let boy2 = "Sushant"
+
+let sentence =  `${boy1} is a friend of ${boy2}`
+console.log(sentence)
+
+//escape sequence characters, they are treated as single char
+let fruit = "Man\'go"
+console.log(fruit)
+let person = "Eima\nLama"
+console.log(person)
+let quote = "He said,\"I am a person\""
+console.log(quote)
