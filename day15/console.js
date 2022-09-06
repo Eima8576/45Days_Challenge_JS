1989eab448d6e212a4e9d58b32bbeff3c5c06b1e
Eima8576 <90395382+Eima8576@users.noreply.github.com>
2022-09-06 16:44:51 +0545

    Add files via upload

---
+++ day15/console.js
@@ -0,0 +1,13 @@
+console.log("Hi This is day 15!")
+console.error("This is error!!")
+console.assert(776>90)
+console.clear()
+
+//console.table
+let a = {x: 2, y: 4, z: 8}
+console.table(a) //creates table and shows key-value of a
+
+console.warn("Don't drink and drive!")
+console.info("Hi! This is 45 days challenge of CLG Askmitra")
+console.time("this")
+console.timeEnd("this")
