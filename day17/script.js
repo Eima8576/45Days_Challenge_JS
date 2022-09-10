00f1c78dee7ec0fac30ada264f5127acc15e39b8
Eima8576 <90395382+Eima8576@users.noreply.github.com>
2022-09-10 10:55:10 +0545

    Add files via upload

---
+++ day17/script.js
@@ -0,0 +1,7 @@
+//window object in js
+console.log(window)
+console.log(document.body)
+document.body.style.background = "green"
+//BOM example
+alert("Virus!")
+location.href = "https://askmitra.com/clg"
